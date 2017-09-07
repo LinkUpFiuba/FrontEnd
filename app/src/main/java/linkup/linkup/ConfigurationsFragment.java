2f365f85541e925edce597767e10679fff8f6ea1
germanchaca <germanchaca@gmail.com>
2017-09-07 13:05:42 -0300

    #24 Iconos

--- app/src/main/java/linkup/linkup/ConfigurationsFragment.java
+++ app/src/main/java/linkup/linkup/ConfigurationsFragment.java
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import linkup.linkup.helper.CropCircleTransformation;
 
 
 public class ConfigurationsFragment extends Fragment {
@@ -18,6 +23,8 @@ public class ConfigurationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_configurations, container, false);
+        ImageView profileImageView = (ImageView) view.findViewById(R.id.profileImage);
+        Picasso.with(getContext()).load(R.drawable.p2).fit().centerCrop().transform(new CropCircleTransformation()).into(profileImageView);
         return view;
     }
 }
