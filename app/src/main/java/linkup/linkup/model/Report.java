65faaa7b22e32559d6611a702492c99b11ea8567
andres <oteroandres95@gmail.com>
2017-10-07 18:07:39 -0300

    Denuncia con timestamp

--- app/src/main/java/linkup/linkup/model/Report.java
+++ app/src/main/java/linkup/linkup/model/Report.java
@@ -2,12 +2,15 @@ package linkup.linkup.model;
 
 import com.google.firebase.database.Exclude;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by andres on 9/28/17.
  */
 
 public class Report {
-    public String idReporting,message,state;
+    public String idReporting,message,state,timeStamp;
     @Exclude
     public String idReported;
     public Report(){}
@@ -16,5 +19,7 @@ public class Report {
         this.idReporting=idReporting;
         this.message=message;
         this.state="pending";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.timeStamp=dateFormat.format(new Date());
     }
 }
