84ca9877055ac8a707bbada3fb09f0d157d7f659
andres <oteroandres95@gmail.com>
2017-10-29 12:08:30 -0300

    Fix

--- app/src/main/java/linkup/linkup/model/Photo.java
+++ app/src/main/java/linkup/linkup/model/Photo.java
@@ -25,8 +25,8 @@ public class Photo implements Serializable {
         for(int i=0; i<photoArray.length();i++){
             Photo photo=new Photo();
             JSONObject photoObject = photoArray.getJSONObject(i);
-            if(photoObject.has("id")){
-                photo.url=photoObject.getString("id");
+            if(photoObject.has("url")){
+                photo.url=photoObject.getString("url");
                 photoList.add(photo);
             }
         }
